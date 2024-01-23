f6632de1f6f072dfa2486524e844e7b62d245b71
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-01-23 18:08:42 +0900

    feat: Add comment in chap02

--- src/sec02/chap02/Main.java
+++ src/sec02/chap02/Main.java
@@ -11,5 +11,18 @@ public class Main {
 
         //  한 줄짜리 주석
         System.out.println("오른쪽을 봐"); // 주석이 요기잉네?
+
+        /* 주석은 이와 같이 여러 줄에 걸쳐
+        작성할 수도 있습니다.
+        장문의 주석을 달거나
+
+        System.out.println("아...!");
+        System.out.println("안돼!!");
+
+        위와 같이
+        코드의 일부를 주석처리하여
+        비활성화는 등의 목적으로 사용되죠. */
+
+        System.out.println("돼.");
     }
 }
