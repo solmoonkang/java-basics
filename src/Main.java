91dcb7ac142897749db583a986f8d1d5d9a679a2
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-01-23 17:45:37 +0900

    Init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {    // 프로그램이 시작되는 곳
-        System.out.println("Hello world!");     // 개발하면서 값을 출력해보는 기능
+        System.out.println("안녕하세요!");     // 개발하면서 값을 출력해보는 기능
     }
 }
