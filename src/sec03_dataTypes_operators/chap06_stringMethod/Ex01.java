cae79e88ad7820013d653bbb879807f855862748
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-11 20:53:16 +0900

    feat: Add returning String length in chapter 06 string method

--- src/sec03_dataTypes_operators/chap06_stringMethod/Ex01.java
+++ src/sec03_dataTypes_operators/chap06_stringMethod/Ex01.java
@@ -7,5 +7,9 @@ public class Ex01 {
         int int2 = "헬로".length();
         int int3 = "Hello".length();
         int int4 = "김수한무 거북이와 두루미".length();
+
+        int int5 = "안녕하세요.".length();
+        String str = "Hello World";
+        int int6 = str.length();
     }
 }
