65ebe1f9331d3fa85eb6730d12ac662364c3f404
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-14 18:02:07 +0900

    feat: Add break statement only at the end in chapter 02 switch case

---
+++ src/section04/controlstate_methods/chapter02/switchcase/Ex03.java
@@ -0,0 +1,19 @@
+package section04.controlstate_methods.chapter02.switchcase;
+
+public class Ex03 {
+    public static void main(String[] args) {
+
+        //  💡 break 관련 동작방식을 이용
+        char yutnori = '도';
+
+        switch (yutnori) {
+            case '모': System.out.println("앞으로");
+            case '윷': System.out.println("앞으로");
+            case '걸': System.out.println("앞으로");
+            case '개': System.out.println("앞으로");
+            case '도': System.out.println("앞으로"); break;
+            default:
+                System.out.println("무효");
+        }
+    }
+}
