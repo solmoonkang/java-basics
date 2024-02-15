e12ab59e8b80bb2271ca50511e821d30da152b04
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-15 17:26:45 +0900

    feat: Add using scanner for other data type input in chapter 07 input

---
+++ src/section04/controlstate_methods/chapter07/input/Ex02.java
@@ -0,0 +1,19 @@
+package section04.controlstate_methods.chapter07.input;
+
+import java.util.Scanner;
+
+public class Ex02 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        boolean bool = sc.nextBoolean();
+        int intNum = sc.nextInt();
+        double dblNum = sc.nextDouble();
+        // 🧪 기타 next~ 메서드들 확인해 볼 것
+
+        System.out.println("bool: " + bool);
+        System.out.println("intNum: " + intNum);
+        System.out.println("dblNum: " + dblNum);
+    }
+}
