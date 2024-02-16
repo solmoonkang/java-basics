db2baa6396d496afa0e3e8de4318bcb85ab3cf39
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-16 10:29:01 +0900

    feat: Implement slime class in oop section chapter 02 application basic

---
+++ src/section05/oop/chapter02/application/ex01/Main.java
@@ -0,0 +1,11 @@
+package section05.oop.chapter02.application.ex01;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Slime slime1 = new Slime();
+        Slime slime2 = new Slime();
+
+        slime1.attack(slime2);
+    }
+}
