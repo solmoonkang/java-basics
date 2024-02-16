6b8eb133168637df3eec6ddbcbfda993ce4ead93
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-16 10:50:08 +0900

    feat: Implement automatic generation of store numbers in oop section chapter 03 static field and method

---
+++ src/section05/oop/chapter03/staticclass/ex02/Main.java
@@ -0,0 +1,10 @@
+package section05.oop.chapter03.staticclass.ex02;
+
+public class Main {
+    public static void main(String[] args) {
+
+        YalcoChicken store1 = new YalcoChicken("판교");
+        YalcoChicken store2 = new YalcoChicken("강남");
+        YalcoChicken store3 = new YalcoChicken("제주");
+    }
+}
