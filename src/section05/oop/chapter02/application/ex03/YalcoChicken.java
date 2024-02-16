f6388f47c5d4e5664aebb0cd2f676931dfc9a60c
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-16 10:39:58 +0900

    feat: Implement yalco chicken and chicken menu classes in oop section chapter 02 application basic

---
+++ src/section05/oop/chapter02/application/ex03/YalcoChicken.java
@@ -0,0 +1,22 @@
+package section05.oop.chapter02.application.ex03;
+
+public class YalcoChicken {
+    int no;
+    String name;
+    ChickenMenu[] menus;
+
+    YalcoChicken (int no, String name, ChickenMenu[] menus) {
+        this.no = no;
+        this.name = name;
+        this.menus = menus;
+    }
+
+    ChickenMenu orderMenu (String name) {
+        for (ChickenMenu menu : menus) {
+            if (menu.name.equals(name)) { // 🔴
+                return menu;
+            }
+        }
+        return null;
+    }
+}
