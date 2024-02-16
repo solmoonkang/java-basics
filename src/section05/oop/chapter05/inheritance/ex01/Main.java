603bee9f106f48edcbe02359b5eb348218231de8
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-16 12:04:18 +0900

    feat: Implement use of extends operator in oop section chapter 05 inheritance

---
+++ src/section05/oop/chapter05/inheritance/ex01/Main.java
@@ -0,0 +1,14 @@
+package section05.oop.chapter05.inheritance.ex01;
+
+public class Main {
+    public static void main(String[] args) {
+
+        YalcoChickenDT dtStore1 = new YalcoChickenDT(108, "철원");
+
+        dtStore1.takeHallOrder();
+
+        dtStore1.takeDTOrder();
+        dtStore1.setDriveThruOpen(false);
+        dtStore1.takeDTOrder();
+    }
+}
