b96f5a21a0aa43b6f79d16a905892c7681a2a29a
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-16 12:31:41 +0900

    feat: Implement concept of polymorphism in oop section chapter 06 polymorphism

---
+++ src/section05/oop/chapter06/polymorphism/ex01/Button.java
@@ -0,0 +1,14 @@
+package section05.oop.chapter06.polymorphism.ex01;
+
+public class Button {
+
+    private String print;
+
+    public Button(String print) {
+        this.print = print;
+    }
+
+    public void func () {
+        System.out.println(print + " 입력 적용");
+    }
+}
