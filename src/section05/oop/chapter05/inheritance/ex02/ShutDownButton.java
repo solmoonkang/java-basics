e6514b9ecd3ac68a969f7781f67ebdf308838781
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-16 12:11:49 +0900

    feat: Implement method overriding and use of super keyword in oop section chapter 05 inheritance

---
+++ src/section05/oop/chapter05/inheritance/ex02/ShutDownButton.java
@@ -0,0 +1,14 @@
+package section05.oop.chapter05.inheritance.ex02;
+
+public class ShutDownButton extends Button {
+
+    public ShutDownButton () {
+        super("ShutDown"); // 💡 부모의 생성자 호출 -> 부모 클래스인 Button의 생성자를 호출해서 자기 자신을 생성한다.
+    }
+
+    //  💡 부모의 메소드를 override
+    @Override
+    public void func () {
+        System.out.println("프로그램 종료");
+    }
+}
