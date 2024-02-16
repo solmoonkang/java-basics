83842029958b2c8a4eeb7946a1ff3dd7c4a8ca21
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-16 11:16:42 +0900

    feat: Implement concept of access modifiers in oop section chapter 04 access modifiers

---
+++ src/section05/oop/chapter04/accessmodifiers/ex01/SmartPhone.java
@@ -0,0 +1,7 @@
+package section05.oop.chapter04.accessmodifiers.ex01;
+
+public class SmartPhone {
+    String powerButton = "OnOff";
+    public String sdCardSlot = "SD Card";
+    private String cpu = "Yalcom";
+}
