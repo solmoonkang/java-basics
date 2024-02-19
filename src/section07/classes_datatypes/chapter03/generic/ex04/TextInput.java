58094f974df33c8be2e64dec32b93407d4fc0920
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-19 17:31:42 +0900

    feat: Implement bounded generics in section object chapter 03 generic

---
+++ src/section07/classes_datatypes/chapter03/generic/ex04/TextInput.java
@@ -0,0 +1,9 @@
+package section07.classes_datatypes.chapter03.generic.ex04;
+
+public class TextInput extends FormElement {
+
+    @Override
+    void func() {
+        System.out.println("텍스트 입력 받음");
+    }
+}
