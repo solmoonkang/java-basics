68e06707168d973e7281db998d679afc183842b0
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-19 12:45:49 +0900

    feat: Implement how to use records in classes section chapter 07 records

---
+++ src/section06/classes/chapter07/records/ex01/Gender.java
@@ -0,0 +1,10 @@
+package section06.classes.chapter07.records.ex01;
+
+public enum Gender {
+
+    MALE("👦🏻"), FEMALE("👧🏼");
+
+    private String emoji;
+    Gender(String emoji) { this.emoji = emoji; }
+    public String getEmoji() { return emoji; }
+}
