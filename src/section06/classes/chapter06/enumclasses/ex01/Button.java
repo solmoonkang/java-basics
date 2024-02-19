f91820443dfce3eb5173bcdc3380a60e8be57b3d
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-19 12:33:25 +0900

    feat: Implement use of enum class in classes section chapter 06 enum classes

---
+++ src/section06/classes/chapter06/enumclasses/ex01/Button.java
@@ -0,0 +1,15 @@
+package section06.classes.chapter06.enumclasses.ex01;
+
+public class Button {
+
+    private ButtonMode buttonMode = ButtonMode.LIGHT;
+    private ButtonSpace buttonSpace = ButtonSpace.SINGLE;
+
+    public void setButtonMode(ButtonMode buttonMode) {
+        this.buttonMode = buttonMode;
+    }
+
+    public void setButtonSpace(ButtonSpace buttonSpace) {
+        this.buttonSpace = buttonSpace;
+    }
+}
