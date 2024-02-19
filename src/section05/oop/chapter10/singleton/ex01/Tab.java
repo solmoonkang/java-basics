509e43dc471fe3d6d24953991904e4855772b0a6
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-19 10:38:52 +0900

    feat: Implement singleton pattern in oop section chapter 10 singleton

---
+++ src/section05/oop/chapter10/singleton/ex01/Tab.java
@@ -0,0 +1,10 @@
+package section05.oop.chapter10.singleton.ex01;
+
+public class Tab {
+
+    private Setting setting = new Setting();
+
+    public Setting getSetting() {
+        return setting;
+    }
+}
