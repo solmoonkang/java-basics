99f5b89bf303f0a89e40f361d8877e94889aa197
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-19 12:33:59 +0900

    feat: Implement misuse occurs when used inside a class in classes section chapter 06 enum classes

---
+++ src/section06/classes/chapter06/enumclasses/ex02/Main.java
@@ -0,0 +1,11 @@
+package section06.classes.chapter06.enumclasses.ex02;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Button button1 = new Button();
+
+        button1.setMode(Button.Mode.DARK);
+        button1.setSpace(Button.Space.DOUBLE);
+    }
+}
