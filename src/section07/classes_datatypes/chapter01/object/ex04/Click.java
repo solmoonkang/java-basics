d4058e5cabf7c25c5d5b60849d2ca316a36042d5
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-19 15:18:12 +0900

    feat: Implement use of clone methods in section object chapter 01 object classes

---
+++ src/section07/classes_datatypes/chapter01/object/ex04/Click.java
@@ -0,0 +1,12 @@
+package section07.classes_datatypes.chapter01.object.ex04;
+
+public class Click {
+
+    int x;
+    int y;
+
+    public Click(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
