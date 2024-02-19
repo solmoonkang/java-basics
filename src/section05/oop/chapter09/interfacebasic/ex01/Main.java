225ddbac9fb544666c450130e886634fda13b5c1
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-19 10:27:14 +0900

    feat: Implement difference between abstract class and interface in oop section chapter 09 interface basic

---
+++ src/section05/oop/chapter09/interfacebasic/ex01/Main.java
@@ -0,0 +1,23 @@
+package section05.oop.chapter09.interfacebasic.ex01;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //  ⭐ 다형성
+        PolarBear polarBear = new PolarBear();
+        Mamal mamal = polarBear;
+        Swimmer swimmer = polarBear;
+
+        GlidingLizard glidingLizard = new GlidingLizard();
+        Eagle eagle = new Eagle();
+
+        Hunter[] hunters = {
+                polarBear, glidingLizard, eagle
+        };
+
+        //  💡 인터페이스 역시 다형성에 의해 자료형으로 작용 가능
+        for (Hunter hunter : hunters) {
+            hunter.hunt();
+        }
+    }
+}
