16bbf7a91ec69241d5ac7521c2e792e9a9dc070e
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-19 12:12:43 +0900

    feat: Implement utilization of main method in classes section chapter 05 main method

---
+++ src/section06/classes/chapter05/mainmethod/Ex01.java
@@ -0,0 +1,8 @@
+package section06.classes.chapter05.mainmethod; // ⭐️ 파일이 든 폴더에서 실행하려면 지울 것!
+
+public class Ex01 {
+    public static void main(String[] args) {
+
+        System.out.println("메인 메소드 실행");
+    }
+}
