4f76242cae473dc6a9bfc56896e515b87e08ac00
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-19 17:48:52 +0900

    feat: Implement side class representing factions in section object chapter 04 example of game

---
+++ src/section07/classes_datatypes/chapter04/example_game/Side.java
@@ -0,0 +1,11 @@
+package section07.classes_datatypes.chapter04.example_game;
+
+public enum Side {
+
+    RED("레드"), BLUE("블루");
+    private String name;
+
+    Side(String name) { this.name = name; }
+
+    public String getName() { return name; }
+}
