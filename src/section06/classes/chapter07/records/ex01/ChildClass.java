44cf43b6fdb55a488e3967b9a2abdde5ae5186bb
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-19 12:45:55 +0900

    feat: Implement how to use records in classes section chapter 07 records

---
+++ src/section06/classes/chapter07/records/ex01/ChildClass.java
@@ -0,0 +1,19 @@
+package section06.classes.chapter07.records.ex01;
+
+//  기존처럼 클래스로 작성해야 했다면...
+public class ChildClass {
+
+    private final String name;
+    private final int birthYear;
+    private final Gender gender;
+
+    public ChildClass(String name, int birthYear, Gender gender) {
+        this.name = name;
+        this.birthYear = birthYear;
+        this.gender = gender;
+    }
+
+    public String getName() { return name; }
+    public int getBirthYear() { return birthYear; }
+    public Gender getGender() { return gender; }
+}
