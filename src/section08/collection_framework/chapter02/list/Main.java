a3446e98b8956e370e3606f98e56436c54a0fbdf
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-19 18:32:46 +0900

    feat: Implement declaration and usage of linkedList in section collection framework chapter 02 list

--- src/section08/collection_framework/chapter02/list/Main.java
+++ src/section08/collection_framework/chapter02/list/Main.java
@@ -112,5 +112,71 @@ public class Main {
         //  요소가 하나 지워졌는지 여부 반환
         boolean removed1 = knights.remove(new Knight(Side.RED));
         boolean removed2 = knights.remove(knight1);
+
+
+
+        //  ⭐️ 둘의 차이와 연관지어 생각해 볼 것
+
+        //  💡 ArrayList에만 있는 메소드들 중...
+        ArrayList<Attacker> attackers = new ArrayList<>();
+        //  자주 쓰이지는 않음
+        attackers.ensureCapacity(5); // 자리수 미리 확보
+        attackers.trimToSize(); // 남는 자리 없애기 (메모리 회수)
+
+
+        //  💡 LinkedList에만 있는 메소드들 중...
+        LinkedList<Integer> intNums = new LinkedList<>();
+        for (int intNum : new int[] {2, 3, 4}) { intNums.add(intNum); };
+
+        intNums.addFirst(1);
+        intNums.addFirst(0);
+        intNums.addLast(5); // add와 반환값만 다름. 코드에서 확인해 볼 것
+        intNums.addLast(6);
+
+
+        //  💡 앞에서/뒤에서 꺼내지 않고 반환
+        //  - peek~ : 비어있을 경우 null 반환
+        //  - get~ : 비어있을 경우 익셉션
+        int peekedFirst = intNums.peekFirst();
+        int gottenFirst = intNums.getFirst();
+        int peekedLast = intNums.peekLast();
+        int gottenLast = intNums.getLast();
+
+
+        //  💡 앞에서/뒤에서 꺼내어 반환
+        //  - poll~ : 비어있을 경우 null 반환
+        //  - remove~ : 비어있을 경우 익셉션
+        int polledFirst = intNums.pollFirst();
+        int removedSecond = intNums.removeFirst();
+        int polledLast = intNums.pollLast();
+        int removedLast = intNums.removeLast();
+
+        //  ⭐️ 위의 기능들 활용하여 Stack/Queue 구현
+
+
+        LinkedList<Character> charLList = new LinkedList<>();
+
+        //  💡 push & pop : 스택 간편하게 구현
+        //  - 클래스 코드에서 살펴볼 것
+
+        charLList.push('A');
+        charLList.push('B');
+        charLList.push('C');
+        charLList.push('D');
+        charLList.push('E');
+
+        char pop1 = charLList.pop();
+        char pop2 = charLList.pop();
+        char pop3 = charLList.pop();
+
+
+
+        List<Integer> list1 = Arrays.asList(1, 2, 3, 4, 5);
+        ArrayList<Integer> list2 = new ArrayList<>(list1);
+
+        String list1Type = list1.getClass().getName();
+        String list2Type = list2.getClass().getName();
+
+        // list1.add(6); // ⚠️ 런타임 오류
     }
 }
