e6792150b2c5bd94c8d38521142c325a4dc33b96
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-19 12:05:13 +0900

    feat: Implement utilization of anonymous classes in classes section chapter 04 anonymous classes

---
+++ src/section06/classes/chapter04/anonymousclasses/ex02/Button.java
@@ -0,0 +1,19 @@
+package section06.classes.chapter04.anonymousclasses.ex02;
+
+public class Button {
+
+    String name;
+    public Button(String name) {
+        this.name = name;
+    }
+
+    //  ⭐️ 인터페이스를 상속한 클래스 자료형
+    private OnClickListener onClickListener;
+    public void setOnClickListener(OnClickListener onClickListener) {
+        this.onClickListener = onClickListener;
+    }
+
+    public void func () {
+        onClickListener.onClick();
+    }
+}
