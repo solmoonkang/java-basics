a01698796e3feb804975341cb89e1aafa94f9f0e
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-19 10:27:08 +0900

    feat: Implement difference between abstract class and interface in oop section chapter 09 interface basic

---
+++ src/section05/oop/chapter09/interfacebasic/ex01/Mamal.java
@@ -0,0 +1,10 @@
+package section05.oop.chapter09.interfacebasic.ex01;
+
+public abstract class Mamal {
+
+    public boolean hibernation;
+
+    public Mamal(boolean hibernation) {
+        this.hibernation = hibernation;
+    }
+}
