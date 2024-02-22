171d5a30c5dfaef69952b1c4cf2cf8ecb1a4c550
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-22 18:31:37 +0900

    feat: Implement exception bubbling in section error handling chapter 04 passing re-throwing

---
+++ src/section10/error_handling/chapter04/passing_rethrowing/MediumException.java
@@ -0,0 +1,8 @@
+package section10.error_handling.chapter04.passing_rethrowing;
+
+public class MediumException extends Exception {
+
+    public MediumException() {
+        super("대리급 문제");
+    }
+}
