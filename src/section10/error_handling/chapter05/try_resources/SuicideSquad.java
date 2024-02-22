2008154295b6fb359fdde70c02f18d5af6a5d84b
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-22 18:39:38 +0900

    feat: Implement and using custom try with resources exception in section error handling chapter 05 try with resources

---
+++ src/section10/error_handling/chapter05/try_resources/SuicideSquad.java
@@ -0,0 +1,18 @@
+package section10.error_handling.chapter05.try_resources;
+
+import java.util.Random;
+
+public class SuicideSquad implements AutoCloseable {
+
+    public void doSecretTask () throws OpFailException {
+        if (new Random().nextBoolean()) {
+            throw new OpFailException();
+        };
+        System.out.println("🔫 비밀 작전 수행");
+    }
+
+    @Override
+    public void close() throws Exception {
+        System.out.println("💣 전원 폭사\n- - - - -\n");
+    }
+}
