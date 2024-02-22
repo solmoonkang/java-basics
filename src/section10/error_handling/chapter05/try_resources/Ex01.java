88ba9c011879be0c1e725a570f37c8df208899e8
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-22 18:37:50 +0900

    feat: Implement using try with resources in section error handling chapter 05 try with resources

---
+++ src/section10/error_handling/chapter05/try_resources/Ex01.java
@@ -0,0 +1,58 @@
+package section10.error_handling.chapter05.try_resources;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Ex01 {
+    public static void main(String[] args) {
+
+        String correctPath = "./src/sec09/chap04/turtle.txt";
+        String wrongPath = "./src/sec09/chap04/rabbit.txt";
+
+        openFile1(correctPath);
+        openFile1(wrongPath);
+
+
+
+        System.out.println("\n- - - - -\n");
+
+        openFile2(correctPath);
+        openFile2(wrongPath);
+    }
+
+    public static void openFile1 (String path) {
+        Scanner scanner = null;
+
+        try {
+            scanner = new Scanner(new File(path));
+            while (scanner.hasNextLine()) {
+                System.out.println(scanner.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.out.printf("⚠️ %s 파일 없음%n", path);
+        } finally {
+            System.out.println("열었으면 닫아야지 ㅇㅇ");
+            if (scanner != null) scanner.close();
+
+            //  💡 만약 이 부분을 작성하는 것을 잊는다면?
+        }
+    }
+
+
+    public static void openFile2 (String path) {
+        //  ⭐ Scanner가 Closable - AutoClosable를 구현함 확인
+
+        try (Scanner scanner = new Scanner(new File(path))) {
+            while (scanner.hasNextLine()) {
+                System.out.println(scanner.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.out.printf("⚠️ %s 파일 없음%n", path);
+        }
+
+        // 💡 .close를 작성하지 않아도 자동으로 호출됨
+    }
+}
