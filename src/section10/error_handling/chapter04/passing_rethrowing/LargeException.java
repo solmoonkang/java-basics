d62f63576071b1a0f47264ac5d0b2ef4f7bb2578
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-22 18:31:39 +0900

    feat: Implement exception bubbling in section error handling chapter 04 passing re-throwing

---
+++ src/section10/error_handling/chapter04/passing_rethrowing/LargeException.java
@@ -0,0 +1,8 @@
+package section10.error_handling.chapter04.passing_rethrowing;
+
+public class LargeException extends Exception {
+
+    public LargeException() {
+        super("부장급 문제");
+    }
+}
