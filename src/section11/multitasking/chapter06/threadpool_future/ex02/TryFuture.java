213ababe1d3ce7a2a432d1e167102c7296e24d64
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-23 11:59:12 +0900

    feat: Implement using Callable in section multitasking chapter 06 thread-poll and future

---
+++ src/section11/multitasking/chapter06/threadpool_future/ex02/TryFuture.java
@@ -0,0 +1,39 @@
+package section11.multitasking.chapter06.threadpool_future.ex02;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.stream.IntStream;
+
+public class TryFuture {
+    public static void main(String[] args) {
+
+        //  💡 쓰레드 풀과 Future를 사용해서 여러 Callable 동시에 실행
+        ExecutorService es = Executors.newFixedThreadPool(4);
+
+        List<Future<Integer>> futList = new ArrayList<>();
+        IntStream.range(0, 10)
+                .forEach(i -> {
+                    futList.add(
+                            es.submit(new RollDiceCall())
+                    );
+                });
+
+        es.shutdown(); // 💡 제거하면 프로그램이 끝나지 않음
+
+        ArrayList<Integer> intList = new ArrayList<>();
+        for (Future<Integer> future : futList) {
+            try {
+                intList.add(future.get());
+            } catch (InterruptedException | ExecutionException e) {}
+        }
+
+        System.out.println(String.join(
+                ",",
+                intList.stream().map(String::valueOf).toArray(String[]::new)
+        ));
+    }
+}
