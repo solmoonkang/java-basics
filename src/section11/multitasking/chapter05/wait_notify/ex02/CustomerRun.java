3aa094913037488ddb69a53e5e2bf7c698145e6c
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-23 11:57:35 +0900

    feat: Implement producer and consumer model in section multitasking chapter 05 wait and notify

---
+++ src/section11/multitasking/chapter05/wait_notify/ex02/CustomerRun.java
@@ -0,0 +1,19 @@
+package section11.multitasking.chapter05.wait_notify.ex02;
+
+public class CustomerRun implements Runnable {
+
+    String name;
+    CoffeeMachine coffeeMachine;
+
+    public CustomerRun(String name, CoffeeMachine coffeeMachine) {
+        this.name = name;
+        this.coffeeMachine = coffeeMachine;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            coffeeMachine.takeout(this);
+        }
+    }
+}
